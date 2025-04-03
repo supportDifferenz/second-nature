683a132f34f95aa6ea9b50233571cb7d3f026cbe
shibil06 <shibil.salim@diffrenz.com>
2025-04-03 12:49:10 +0530

    Shibil UI (#14)
    * Added About US page to project
    
    * Merge branch 'main' of https://github.com/farismannengal/second-nature into shibilUi
    
    * Added more components and enhanments to aboutUs and Meals menu page
    
    * About us pages added and conflict issues
    
    * Added behind the scene section

---
+++ src/app/behind-the-scenes/page.tsx
@@ -0,0 +1,12 @@
+import GuidelinesCard from '@/components/pages/behindTheScenes/GuidelinesCard'
+import HeroSection from '@/components/pages/behindTheScenes/HeroSection'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <HeroSection/>
+        <GuidelinesCard/>
+    </div>
+  )
+}
