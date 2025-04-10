45abbc1ce00a1289ccb7304b604a33a4c35916a5
shibil06 <shibil.salim@diffrenz.com>
2025-04-10 09:46:06 +0530

    Added character of pets section

--- src/app/(buying-flow)/(step-2)/activity/page.tsx
+++ src/app/(buying-flow)/(step-2)/activity/page.tsx
@@ -1,16 +1,42 @@
-import Typography from '@/components/atoms/typography/Typography'
-import BuyingFlowLayout from '@/components/templates/BuyingFlowLayout'
-import React from 'react'
+"use client";
+import Typography from "@/components/atoms/typography/Typography";
+import BuyingFlowLayout from "@/components/templates/BuyingFlowLayout";
+import { useState } from "react";
+import PetActiveCard from "@/components/molecules/petActiveCard/PetActiveCard";
 
-export default function page() {
+export default function Page() {
+  const [selectedPet, setSelectedPet] = useState<
+    "sedentary" | "less-active" | "active" | ""
+  >("");
   return (
     <BuyingFlowLayout step={2}>
-      <div className="flex flex-col items-center gap-8 bg-white"  >
-        <Typography tag="h3" text="Jackey’s Weight and Target Weight" className="text-center text-primary-dark" />
-        <div className="w-full mx-auto items-center justify-center flex flex-wrap lg:gap-14 ">
-        
+      <div className="flex flex-col items-center gap-8 bg-white">
+        <Typography
+          tag="h3"
+          text="How active is Jackey?"
+          className="text-center text-primary-dark"
+        />
+        <div className="w-full mx-auto items-center justify-center flex flex-wrap lg:gap-[var(--space-20-60)] ">
+          <PetActiveCard
+            image="/images/sedentary.webp"
+            text="Sedentary"
+            selectedPet={selectedPet === "sedentary"}
+            setSelectedCharacter={() => setSelectedPet("sedentary")}
+          />
+          <PetActiveCard
+            image="/images/less-acive.webp"
+            text="Sedentary"
+            selectedPet={selectedPet === "less-active"}
+            setSelectedCharacter={() => setSelectedPet("less-active")}
+          />
+          <PetActiveCard
+            image="/images/active-pet.webp"
+            text="Sedentary"
+            selectedPet={selectedPet === "active"}
+            setSelectedCharacter={() => setSelectedPet("active")}
+          />
         </div>
       </div>
     </BuyingFlowLayout>
-  )
+  );
 }
--- src/components/molecules/petActiveCard/PetActiveCard.tsx
+++ src/components/molecules/petActiveCard/PetActiveCard.tsx
@@ -1,15 +1,52 @@
-import Typography from '@/components/atoms/typography/Typography'
-import React from 'react'
+"use client";
+import Typography from "@/components/atoms/typography/Typography";
+import Image from "next/image";
 
-export default function PetActiveCard() {
+export default function PetActiveCard({
+  image,
+  text,
+  selectedPet,
+  setSelectedCharacter,
+}: {
+  image: string;
+  text: string;
+  selectedPet: boolean;
+  setSelectedCharacter: () => void;
+}) {
   return (
-    <div>
-        <div>
-            image
+    <div
+      className={`relative border rounded-2xl cursor-pointer overflow-hidden ${
+        selectedPet ? "border-secondary-1 border-[1.5px]" : "border-[#E8E8E8]"
+      }`}
+      onClick={setSelectedCharacter}
+    >
+      <div className="flex flex-col gap-[var(--space-27-34)] px-[var(--space-8-13)] pt-9 pb-[var(--space-15-30)] w-[43.57vw] sm:w-[14.06vw] border border-[#E8E8E8] rounded-2xl">
+        <div className="w-[100%]">
+          <Image
+            src={image}
+            alt="mission"
+            className="!static inset-0 w-full !h-full object-cover object-center"
+            fill
+          />
         </div>
-        <div>
-            <Typography tag="h5" text="" className="text-center text-primary-dark" />
+
+        <Typography
+          tag="h6"
+          text={text}
+          className="text-center uppercase text-primary-dark"
+        />
+      </div>
+
+      {selectedPet && (
+        <div className="absolute top-1 sm:top-2 sm:right-2 right-1 w-8 h-8 sm:w-10 sm:h-10 rounded-full p-1">
+          <Image
+            src="/icons/checked.svg"
+            alt={`${text} checked`}
+            fill
+            className="!static w-fit h-full"
+          />
         </div>
+      )}
     </div>
-  )
+  );
 }
