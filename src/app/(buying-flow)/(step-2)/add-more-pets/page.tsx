55d55032cf653ae2e9a39dcaf3e9b870ac2ef35e
VishnuPDiffrenz <vishnu@diffrenz.com>
2025-05-05 16:44:52 +0530

    Add more pets page modifications done based on change in flow

--- src/app/(buying-flow)/(step-2)/add-more-pets/page.tsx
+++ src/app/(buying-flow)/(step-2)/add-more-pets/page.tsx
@@ -2,27 +2,29 @@
 
 import Typography from '@/components/atoms/typography/Typography'
 import BuyingFlowLayout from '@/components/templates/BuyingFlowLayout'
-import React from 'react'
+import React, { useState} from 'react'
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { usePetStore } from "@/zustand/store/petDataStore";
+// import { usePetStore } from "@/zustand/store/petDataStore";
 
 export default function Page() {
 
   const router = useRouter();
 
-  const { selectedPetIndex, noOfPets, setSelectedPetIndex } = usePetStore();
+  const [ checkout, setCheckout ] = useState(false);
 
-  const handleNextPet = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  // const { selectedPetIndex, noOfPets, setSelectedPetIndex } = usePetStore();
 
-    if((selectedPetIndex ?? 0) < noOfPets - 1){
-      setSelectedPetIndex((selectedPetIndex ?? 0) + 1);
-      router.push("/gender")
-    }
+  // const handleNextPet = (e: React.MouseEvent<HTMLDivElement>) => {
+  //   e.preventDefault();
+
+  //   if((selectedPetIndex ?? 0) < noOfPets - 1){
+  //     setSelectedPetIndex((selectedPetIndex ?? 0) + 1);
+  //     router.push("/gender")
+  //   }
     
-  }
+  // }
 
   return (
     <BuyingFlowLayout step={2}>
@@ -34,30 +36,22 @@ export default function Page() {
             className="mx-auto items-center justify-center flex flex-wrap border border-[#A1A1A1] rounded-full px-[var(--space-80-100)] py-[var( --space-10-15)] gap-2.5 lg:gap-3.5 cursor-pointer"
             onClick={(e) => {
               e.preventDefault();
+              setCheckout(true);
+            }}
+          >
+            <Typography tag="h6" text="No, I don't want to add another pet" className="text-center text-primary-dark" />
+            {/* <Typography tag="h3" text="+" className="text-center text-primary-dark" />   */}
+          </div>
+          <div 
+            className="mx-auto items-center justify-center flex flex-wrap border border-[#A1A1A1] rounded-full px-[var(--space-80-100)] py-[var( --space-10-15)] py-3 gap-2.5 lg:gap-3.5 cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
               router.push("/dog-or-cat");
             }}
           >
-            <Typography tag="h6" text="Add more pets" className="text-center text-primary-dark" />
-            <Typography tag="h3" text="+" className="text-center text-primary-dark" />  
+            <Typography tag="h6" text="Yes, I want to add another pet" className="text-center text-primary-dark" />
+            {/* <Typography tag="h3" text="+" className="text-center text-primary-dark" />   */}
           </div>
-          { 
-            (selectedPetIndex ?? 0) < noOfPets - 1
-              &&<div 
-                  className="mx-auto items-center justify-center flex flex-wrap border border-[#A1A1A1] rounded-full px-[var(--space-80-100)] py-[var( --space-10-15)] gap-2.5 lg:gap-3.5 cursor-pointer"
-                  onClick={handleNextPet}
-                >
-                  <Typography tag="h6" text="Next Pet" className="text-center text-primary-dark" />
-                  {/* <Typography tag="h3" text="+" className="text-center text-primary-dark" />   */}
-                  <div className="w-5 relative">
-                    <Image
-                      src="/icons/nextIcon.svg"
-                      alt="icon"
-                      fill
-                      className="!static w-full object-contain"
-                    />
-                  </div>
-                </div>
-          }
           {/* <div 
             className="mx-auto items-center justify-center flex flex-wrap border border-[#A1A1A1] rounded-full px-[var(--space-80-100)] py-[var( --space-10-15)] gap-2.5 lg:gap-3.5 cursor-pointer"
             onClick={handleNextPet}
@@ -79,7 +73,7 @@ export default function Page() {
       <div className="pb-[3dvh] flex justify-end items-center gap-4 lg:gap-0 lg:items-end pt-[3dvh]">
         <Button 
           className="gap-2.5 lg:ml-auto lg:mr-[-55px]" 
-          // disabled
+          disabled={!checkout}
           onClick={(e) => {
             e.preventDefault();
             router.push("/checkout");
