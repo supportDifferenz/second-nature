4c704eaa1632b80fa38b753f210998601959a1d2
VishnuPDiffrenz <vishnu@diffrenz.com>
2025-05-05 15:00:53 +0530

    Buying flow layout added for checkout page

--- src/app/(buying-flow)/(step-3)/checkout/page.tsx
+++ src/app/(buying-flow)/(step-3)/checkout/page.tsx
@@ -1,16 +1,19 @@
+import React from "react";
 import CartSummary from "@/components/pageSections/buyingFlow/step3/paymentFinal/CartSummary";
 import FormSection from "@/components/pageSections/buyingFlow/step3/paymentFinal/FormSection";
-import React from "react";
+import BuyingFlowLayout from "@/components/templates/BuyingFlowLayout";
 
 export default function PaymentFinal() {
   return (
-    <div className="flex flex-col lg:flex-row gap-[30.84vw] lg:gap-[3.8vw]  px-[4.67vw] lg:px-[9.89vw]">
-      <div className="order-2 lg:order-1 flex-1">
-        <FormSection />
+    <BuyingFlowLayout step={3}>
+      <div className="flex flex-col lg:flex-row gap-[30.84vw] lg:gap-[3.8vw]  px-[4.67vw] lg:px-[9.89vw]">
+        <div className="order-2 lg:order-1 flex-1">
+          <FormSection />
+        </div>
+        <div className="order-1 lg:order-2 flex-1">
+          <CartSummary />
+        </div>
       </div>
-      <div className="order-1 lg:order-2 flex-1">
-        <CartSummary />
-      </div>
-    </div>
+    </BuyingFlowLayout>
   );
 }
