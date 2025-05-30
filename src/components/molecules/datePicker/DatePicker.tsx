60a471d726932d81cbf396457d205db66b456265
VishnuPDiffrenz <vishnu@diffrenz.com>
2025-05-30 11:43:55 +0530

    Edit pet information page basic set up done

--- src/components/molecules/datePicker/DatePicker.tsx
+++ src/components/molecules/datePicker/DatePicker.tsx
@@ -8,7 +8,7 @@ import { Calendar } from "@/components/ui/calendar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function DateOfBirthPicker({ dateOfBirth, setDateOfBirth }: { dateOfBirth: string; setDateOfBirth: (date: string) => void }) {
+export default function DateOfBirthPicker({ dateOfBirth, setDateOfBirth, className }: { dateOfBirth: string; setDateOfBirth: (date: string) => void; className?: string }) {
   // const [date, setDate] = useState<Date | undefined>();
   const [open, setOpen] = useState(false); // Control popover manually
 
@@ -25,7 +25,7 @@ export default function DateOfBirthPicker({ dateOfBirth, setDateOfBirth }: { dat
         <Button
           onClick={() => setOpen(!open)}
           className={cn(
-            "text-primary-dark justify-between bg-white hover:bg-white max-w-[380px] w-[90%] sm:w-[40%] lg:w-[30%] hover:border-[#A1A1A1] data-[placeholder]:text-[#9B9B9B]  h-13 sm:h-15   rounded-full border border-[#A1A1A1] outline-none data-[state=open]:border-secondary-1   px-4 py-2",
+            `${className} text-primary-dark justify-between bg-white hover:bg-white max-w-[380px] w-[90%] sm:w-[40%] lg:w-[30%] hover:border-[#A1A1A1] data-[placeholder]:text-[#9B9B9B] h-13 sm:h-15 rounded-full border border-[#A1A1A1] outline-none data-[state=open]:border-secondary-1 px-4 py-2`,
             !dateOfBirth && "text-muted-foreground"
           )}
         >
--- src/components/pageSections/dashboard/editPetInformation/EditPetInfo.tsx
+++ src/components/pageSections/dashboard/editPetInformation/EditPetInfo.tsx
@@ -1,19 +1,27 @@
 import { DropDownLabeled } from "@/components/molecules/dropDownLabeled/DropDownLabeled";
 import { InputLabeled } from "@/components/molecules/inputLabeled/InputLabeled";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Label } from "@/components/ui/label";
+import React,{ useState} from "react";
+import DateOfBirthPicker from "@/components/molecules/datePicker/DatePicker";
+import Counter from "@/components/molecules/counterBuyingFlow/Counter";
 
 interface EditPetInfoProps {
   setIsEditPetInfo: (value: boolean) => void;
 }
 
 export default function EditPetInfo({ setIsEditPetInfo }: EditPetInfoProps) {
+
+  const [ dateOfBirth, setDateOfBirth ] = useState("");
+  const [ month, setMonth ] = useState(0);
+  const [ year, setYear ] = useState(0);
+
   return (
-      <div className="lg:w-[85%] grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4 mt-5 mb-[var(--space-30-60)]">
+      <div className="lg:w-[100%] grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4 mt-5 mb-[var(--space-30-60)]">
         <InputLabeled
           label="Pet Name"
           placeholder="Type your Pet Name"
-          className="sm:col-span-2"
+          // className="sm:col-span-2"
         />
         <DropDownLabeled
           placeholder="Select gender"
@@ -32,7 +40,20 @@ export default function EditPetInfo({ setIsEditPetInfo }: EditPetInfoProps) {
           ]}
         />
         <InputLabeled label="Weight" placeholder="" />
-        <InputLabeled label="Birthday" placeholder="" />
+        {/* <InputLabeled label="Birthday" placeholder="" /> */}
+
+        <div className="w-full">
+          <Label>Birthday</Label>
+          <DateOfBirthPicker dateOfBirth={dateOfBirth} setDateOfBirth={setDateOfBirth} className="!w-full !h-14 !rounded-xl" />
+        </div>
+
+        <div className="flex flex-col">
+          <Label>Age</Label>
+          <div className="flex flex-col lg:flex-row gap-3">
+            <Counter label="Months" min={0} max={12} value={month} setValue={setMonth} colorClass="!text-black" className1="!rounded-xl !h-14 !px-3" className2="!gap-1" className3="!w-7" />
+            <Counter label="Years" min={0} max={100} value={year} setValue={setYear} colorClass="!text-black" className1="!rounded-xl !h-14 !px-3" className2="!gap-1" className3="!w-7" />
+          </div>
+        </div>
 
         <div className="flex gap-3">
           <Button 
