33561364e02f30d16ae769aab04004ef1f60e5c3
faris-m14 <faris@tycaninc.com>
2025-07-04 18:44:58 +0530

    testimonial animation

--- src/components/organism/footerBannerCTA/FooterBannerCTA.tsx
+++ src/components/organism/footerBannerCTA/FooterBannerCTA.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Typography from "@/components/atoms/typography/Typography";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 const footerCTAVariants = cva(
   "relative flex flex-col w-full bg-cover bg-center transition-all",
@@ -50,83 +51,87 @@ const FooterBannerCTA: React.FC<
   align,
 }) => {
 
-  const router = useRouter();
+    const router = useRouter();
 
-  const [activeImage, setActiveImage] = useState(image.web);
+    const [activeImage, setActiveImage] = useState(image.web);
 
-  useEffect(() => {
-    const updateImage = () => setActiveImage(getResponsiveImage(image));
-    updateImage(); // Set correct image on mount
-    window.addEventListener("resize", updateImage);
-    return () => window.removeEventListener("resize", updateImage);
-  }, [image]);
+    useEffect(() => {
+      const updateImage = () => setActiveImage(getResponsiveImage(image));
+      updateImage(); // Set correct image on mount
+      window.addEventListener("resize", updateImage);
+      return () => window.removeEventListener("resize", updateImage);
+    }, [image]);
 
-  return (
-    <section
-      id={id}
-      className={` bg-blue-500 relative py-[35%] sm:py-[9%] lg:py-[6%]`}
-    >
-      <div className="w-full absolute top-0 left-0 h-full">
-        <Image
-          src={activeImage}
-          alt="Footer CTA Banner"
-          fill
-          className="!static w-full !h-full object-cover"
-        />
-      </div>
-      <div
-        className={`relative z-10 container text-white ${footerCTAVariants({
-          align,
-        })}`}
+    return (
+      <motion.section
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
+        id={id}
+        className={` bg-blue-500 relative py-[35%] sm:py-[9%] lg:py-[6%]`}
       >
-        <div
-          className={`max-w-[95%] sm:max-w-[70%] lg:max-w-[70%] ${footerCTAVariants(
-            { align }
-          )}`}
-        >
-          {caption && (
-            <Typography
-              tag="h6"
-              text={caption}
-              className="uppercase subtitle !font-normal"
-              style={{ color: captionColor }}
-            />
-          )}
-
-          {title && <Typography tag="h1" text={title} className="highlight capitalize" />}
-
-          {subTitle && (
-            <Typography
-              tag="h3"
-              text={subTitle}
-              className="supporting capitalize mb-(--space-20-30)"
-            />
-          )}
-          {paragraph && (
-            <Typography tag="h6" text={paragraph} style={{ color: paragraphColor }} />
-          )}
-          {subParagraph && (
-            <Typography tag="p" text={subParagraph} className="!text-sm mt-(--space-24-45)" style={{ color: paragraphColor }} />
-          )}
-          {buttonText && buttonLink && (
-            <Button
-              variant={"whiteBtnSecondary2BorderAndText"}
-              size={"md"}
-              // className="mt-(--space-24-45)"
-              className="mt-3.5 lg:mt-2.5 bg-primary-light"
-              onClick={() => {
-                startTransition(() => {
-                  router.push(buttonLink);
-                })
-              }}
-            >
-              {buttonText}
-            </Button>
-          )}
+        <div className="w-full absolute top-0 left-0 h-full">
+          <Image
+            src={activeImage}
+            alt="Footer CTA Banner"
+            fill
+            className="!static w-full !h-full object-cover"
+          />
         </div>
-      </div>
-    </section>
-  );
-};
+        <div
+          className={`relative z-10 container text-white ${footerCTAVariants({
+            align,
+          })}`}
+        >
+          <div
+            className={`max-w-[95%] sm:max-w-[70%] lg:max-w-[70%] ${footerCTAVariants(
+              { align }
+            )}`}
+          >
+            {caption && (
+              <Typography
+                tag="h6"
+                text={caption}
+                className="uppercase subtitle !font-normal"
+                style={{ color: captionColor }}
+              />
+            )}
+
+            {title && <Typography tag="h1" text={title} className="highlight capitalize" />}
+
+            {subTitle && (
+              <Typography
+                tag="h3"
+                text={subTitle}
+                className="supporting capitalize mb-(--space-20-30)"
+              />
+            )}
+            {paragraph && (
+              <Typography tag="h6" text={paragraph} style={{ color: paragraphColor }} />
+            )}
+            {subParagraph && (
+              <Typography tag="p" text={subParagraph} className="!text-sm mt-(--space-24-45)" style={{ color: paragraphColor }} />
+            )}
+            {buttonText && buttonLink && (
+              <Button
+                variant={"whiteBtnSecondary2BorderAndText"}
+                size={"md"}
+                // className="mt-(--space-24-45)"
+                className="mt-3.5 lg:mt-2.5 bg-primary-light"
+                onClick={() => {
+                  startTransition(() => {
+                    router.push(buttonLink);
+                  })
+                }}
+              >
+                {buttonText}
+              </Button>
+            )}
+          </div>
+        </div>
+      </motion.section>
+    );
+  };
 
 export default FooterBannerCTA;
